2edf3585eda46ad6bbe8f0d42e4fba29e55036db
Tod Haren <tod.haren@gmail.com>
2019-02-04 18:23:19 -0800

    Create FVS_Variant class. Add property methods to return FVS variables and arrays.

--- pyfvs2/pyfvs_ctypes.py
+++ pyfvs2/pyfvs_ctypes.py
@@ -4,109 +4,146 @@ Python FVS wrappers using ctypes.
 
 import ctypes as ct
 
-class fchar(ct.Structure):
-	_fields_ = [
-		('str', ct.c_char_p),
-		('len', ct.c_int)
-		]
+import numpy as np
+import pandas as pd
 
-MAXTRE = 3000
-MAXSP = 39
+from commons_ctypes import *
 
-class FVS_glblcntl(ct.Structure):
-	_fields_ = [
-		('firstwrite', ct.c_int),
-		('fvsrtncode', ct.c_int),
-		('jdstash', ct.c_int),
-		('jstash', ct.c_int),
-		('majorstopptcode', ct.c_int),
-		('majorstopptyear', ct.c_int),
-		('maxstoppts', ct.c_int),
-		('minorstopptcode', ct.c_int),
-		('minorstopptyear', ct.c_int),
-		('oldstopyr', ct.c_int),
-		('originalrestartcode', ct.c_int),
-		('readfilepos', ct.c_int),
-		('restartcode', ct.c_int),
-		('seekreadpos', ct.c_int),
-		('stopstatcd', ct.c_int),
-		]
+pd.options.display.float_format = '{:.3f}'.format
 
-class FVS_glblcntlc(ct.Structure):
-	_fields_ = [
-		('keywordfile', ct.c_char * 250),
-		('restartfilefile', ct.c_char * 250),
-		('stopfile', ct.c_char * 250),
-		]
-
-F_REAL = ct.c_float
-F_INTEGER = ct.c_int
-F_LOGICAL = ct.c_int
-class FVS_arrays(ct.Structure):
-	_fields_ = [
-		('abirth', F_REAL * MAXTRE),
-		('bfv', F_REAL * MAXTRE),
-		('cfv', F_REAL * MAXTRE),
-		('crwdth', F_REAL * MAXTRE),
-		('damsev', (F_INTEGER * 6)*MAXTRE),
-		('dbh', F_REAL * MAXTRE),
-		('defect', F_INTEGER * MAXTRE),
-		('dg', F_REAL * MAXTRE),
-		('ht', F_REAL * MAXTRE),
-		('ht2td', (F_REAL * MAXTRE)*2),
-		('htg', F_REAL * MAXTRE),
-		('icr', F_INTEGER * MAXTRE),
-		('idtree', F_INTEGER * MAXTRE),
-		('imc', F_INTEGER * MAXTRE),
-		('ind', F_INTEGER * MAXTRE),
-		('ind1', F_INTEGER * MAXTRE),
-		('ind2', F_INTEGER * MAXTRE),
-		('isp', F_INTEGER * MAXTRE),
-		('ispecl', F_INTEGER * MAXTRE),
-		('itre', F_INTEGER * MAXTRE),
-		('itrunc', F_INTEGER * MAXTRE),
-		('kutkod', F_INTEGER * MAXTRE),
-		('lbirth', F_LOGICAL * MAXTRE),
-		('normht', F_INTEGER * MAXTRE),
-		('oldpct', F_REAL * MAXTRE),
-		('oldrn', F_REAL * MAXTRE),
-		('pct', F_REAL * MAXTRE),
-		('pltsiz', F_REAL * MAXTRE),
-		('prob', F_REAL * MAXTRE),
-		('wk1', F_REAL * MAXTRE),
-		('wk2', F_REAL * MAXTRE),
-		('wk3', F_REAL * MAXTRE),
-		('wk4', F_REAL * MAXTRE),
-		('wk5', F_REAL * MAXTRE),
-		('wk6', F_REAL * MAXTRE),
-		('yrdlos', F_REAL * MAXTRE),
-		('zrand', F_REAL * MAXTRE),
-		]
-
-class FVS_pltchr(ct.Structure):
-	_fields_ = [
-		('cpvref', ct.c_char*10),
-		('dbcn', ct.c_char*40),
-		('fiajsp', (ct.c_char*4)*MAXSP),
-		('jsp', (ct.c_char*4)*MAXSP),
-		('mgmid', ct.c_char*4),
-		('nplt', ct.c_char*26),
-		('nsp', ((ct.c_char*4)*MAXSP)*3),
-		('plnjsp', (ct.c_char*6)*MAXSP),
-		]
-		
 libname = 'libFVS_pn'
 kwdfile = 'c:/workspace/pyfvs2/data/pnt01.key'
 
-fvs = ct.CDLL(libname)
+class FVSLib(object):
+    def __init__(self, variant):
+        self.libname = 'libFVS_{}'.format(variant.lower())
+        self.fvslib = ct.CDLL(self.libname)
+        
+        self._spp_codes = None
+        self._init_commons()
+        
+    def _init_commons(self):    
+        # Register the common data
+        self.glblcntl = FVS_GLBLCNTL.in_dll(self.fvslib, 'glblcntl_')
+        self.glblcntlc = FVS_GLBLCNTLC.in_dll(self.fvslib, 'glblcntlc_')
+        self.arrays = FVS_ARRAYS.in_dll(self.fvslib, 'arrays_')
+        self.pltchr = FVS_PLTCHR.in_dll(self.fvslib, 'pltchr_')
+        self.plot = FVS_PLOT.in_dll(self.fvslib, 'plot_')
+        self.contrl = FVS_CONTRL.in_dll(self.fvslib, 'contrl_')
+        self.outchr = FVS_OUTCHR.in_dll(self.fvslib, 'outchr_')
+        self.outcom  = FVS_OUTCOM.in_dll(self.fvslib, 'outcom_')
 
-# Register the common data
-glblcntl = FVS_glblcntl.in_dll(fvs, 'glblcntl_')
-glblcntlc = FVS_glblcntlc.in_dll(fvs, 'glblcntlc_')
-arrays = FVS_arrays.in_dll(fvs, 'arrays_')
-pltchr = FVS_pltchr.in_dll(fvs, 'pltchr_')
+    def run_kwds(self, kwdfile):
+        self.fvslib.fvssetcmdline_.argtypes = [ct.c_char_p, ct.POINTER(ct.c_int), ct.POINTER(ct.c_int), ct.c_int]
+        self.fvslib.fvs_.argtypes = [ct.POINTER(ct.c_int),]
 
-fvs.fvssetcmdline_.argtypes = [fchar, ct.POINTER(ct.c_int), ct.POINTER(ct.c_int)]
+        cmdline = '--keywordfile={} '.format(kwdfile)
+        lencl = ct.c_int(len(cmdline))
+        rtn_code = ct.c_int(0)
+        self.fvslib.fvssetcmdline_(cmdline.encode(),ct.byref(lencl),ct.byref(rtn_code),len(cmdline))
+        
+        rtn_code = ct.c_int(0)
+        self.fvslib.fvs_(ct.byref(rtn_code))
+    
+    @property
+    def num_cycles(self):
+        """Return the number of cycles in the current run."""
+        return self.contrl.ncyc
+        
+    @property
+    def cycle(self):
+        """Return the current cycle number."""
+        return self.contrl.icyc
+        
+    @property
+    def num_trees(self):
+        """Return the current number of tree records."""
+        return self.contrl.itrn
+        
+    @property
+    def fvs_spp(self):
+        """Return a list of numeric FVS species codes."""
+        return pd.Series([''.join([v.decode() for v in s]).strip() for s in self.pltchr.jsp])
+
+    @property
+    def fia_spp(self):
+        """Return a list of numeric FIA species codes."""
+        return pd.Series([''.join([v.decode() for v in s]) for s in self.pltchr.fiajsp])
+        
+    @property
+    def pln_spp(self):
+        """Return a list of alpha/numeric PLANTS database species codes."""
+        return pd.Series([''.join([v.decode() for v in s]).strip() for s in self.pltchr.plnjsp])
+        
+    @property
+    def spp_codes(self):
+        """
+        Return a Pandas dataframe of the species codes the variant
+        """
+        if self._spp_codes is None:
+            self._spp_codes = pd.DataFrame(dict((
+                    ('spp',self.fvs_spp),
+                    ('fia',self.fia_spp),
+                    ('pln',self.pln_spp)
+                    )))
+        
+        return self._spp_codes
+    
+    @property
+    def dbh(self):
+        """Return a Pandas Series representing the DBH of the current treelist."""
+        return pd.Series(self.arrays.dbh[0:self.contrl.itrn])
+
+    @property
+    def spp(self):
+        """Return a Pandas Series representing the species codes of the current treelist."""
+        return pd.Series(self.arrays.isp[0:self.contrl.itrn])
+    
+    @property
+    def tpa(self):
+        return pd.Series(self.arrays.prob[0:self.contrl.itrn])/self.plot.grospc
+    
+    @property
+    def trees(self):
+        """Return a Pandas DataFrame representing the current treelist."""
+        df = pd.DataFrame(dict((
+            ('treeid', self.arrays.idtree[0:self.contrl.itrn]),
+            ('spp', self.arrays.isp[0:self.contrl.itrn]),
+            ('tpa', self.arrays.prob[0:self.contrl.itrn]),
+            ('mort', self.arrays.wk2[0:self.contrl.itrn]),
+            ('dbh', self.arrays.dbh[0:self.contrl.itrn]),
+            ('dg', self.arrays.dg[0:self.contrl.itrn]),
+            ('ht', self.arrays.ht[0:self.contrl.itrn]),
+            ('htg', self.arrays.htg[0:self.contrl.itrn]),
+            ('cr', self.arrays.icr[0:self.contrl.itrn]),
+            ('cw', self.arrays.crwdth[0:self.contrl.itrn]),
+            ('bapctl', self.arrays.pct[0:self.contrl.itrn]),
+            # ('batlg', self.varcom.ptbalt[0:self.contrl.itrn]),
+            ('tcfv', self.arrays.cfv[0:self.contrl.itrn]),
+            ('mcfv', self.arrays.wk1[0:self.contrl.itrn]),
+            ('mbfv', self.arrays.bfv[0:self.contrl.itrn]),
+            )))
+        
+        df['tpa'] /= self.plot.grospc
+        df['mort'] /= self.plot.grospc
+        
+        return df.sort_values(['spp','treeid'])
+    
+    @property
+    def summary(self):
+        """Return the summary table the current run."""
+        return np.ctypeslib.as_array(fvs.outcom.iosum)[:self.num_cycles+1]
+        
+fvs = FVSLib('pn')
+fvs.run_kwds(kwdfile)
+
+# print(fvs.trees)
+print(fvs.summary)
+
+# print(fvs.outcom.iosum[0:fvs.num_cycles][0:20])
+
+
+# fvs.fvssetcmdline_.argtypes = [fchar, ct.POINTER(ct.c_int), ct.POINTER(ct.c_int)]
 
 # cmdline = '--keywordfile={} '.format(kwdfile)
 # _cmdline = fchar()
@@ -117,13 +154,6 @@ fvs.fvssetcmdline_.argtypes = [fchar, ct.POINTER(ct.c_int), ct.POINTER(ct.c_int)
 # rtn_code = ct.c_int(0)
 # fvs.fvssetcmdline_(_cmdline,ct.byref(lencl),ct.byref(rtn_code))
 
-fvs.fvssetcmdline_.argtypes = [ct.c_char_p, ct.POINTER(ct.c_int), ct.POINTER(ct.c_int), ct.c_int]
-fvs.fvs_.argtypes = [ct.POINTER(ct.c_int),]
-
-cmdline = '--keywordfile={} '.format(kwdfile)
-lencl = ct.c_int(len(cmdline))
-rtn_code = ct.c_int(0)
-fvs.fvssetcmdline_(cmdline.encode(),ct.byref(lencl),ct.byref(rtn_code),len(cmdline))
 
 # # Set the keyword file directly
 # glblcntlc.keywordfile = kwdfile.encode()
@@ -132,17 +162,15 @@ fvs.fvssetcmdline_(cmdline.encode(),ct.byref(lencl),ct.byref(rtn_code),len(cmdli
 
 # print('***', glblcntlc.keywordfile.strip())
 
-rtn_code = ct.c_int(0)
-fvs.fvs_(ct.byref(rtn_code))
 
-print('\n')
-print('DBH', arrays.dbh[0:5])
-print('DG', arrays.dg[0:5])
-print('CFV TD', arrays.ht2td[0][0:5])
-print('BFV TD', arrays.ht2td[1][0:5])
-print('PROB', arrays.prob[0:5])
-print('HT', arrays.ht[0:5])
-print('ISP', arrays.isp[0:5])
+# print('\n')
+# print('DBH', arrays.dbh[0:5])
+# print('DG', arrays.dg[0:5])
+# print('CFV TD', arrays.ht2td[0][0:5])
+# print('BFV TD', arrays.ht2td[1][0:5])
+# print('PROB', arrays.prob[0:5])
+# print('HT', arrays.ht[0:5])
+# print('ISP', arrays.isp[0:5])
 
-print('FIAJSP', [''.join([v.decode() for v in s]) for s in pltchr.fiajsp])
-print('JSP', [''.join([v.decode() for v in s]) for s in pltchr.jsp])
+# print('FIAJSP', [''.join([v.decode() for v in s]) for s in pltchr.fiajsp])
+# print('JSP', [''.join([v.decode() for v in s]) for s in pltchr.jsp])
